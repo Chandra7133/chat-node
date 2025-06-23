b0c1b76dd81f62a6b0d6df7e37070cdc160b7ff7
balaraju <balaraju@bijib.in>
2025-06-23 13:48:27 +0530

    changes

--- app/models/login/index.js
+++ app/models/login/index.js
@@ -19,29 +19,11 @@ exports.signUp = async (reqParams) => {
   created_at = new Date();
   is_verfied = 0;
   const result = await mongoQuery.insertOne(USERS, { username, email, password, is_verfied, created_at });
-  const otpResult = await otpSender({username,email})
+  const otpResult = await otpSender({ username, email })
   // if(otpResult['status']){
   // }else{
   //  return { 'status': SERVICE_UNAVAILABLE_CODE, 'msg': SERVICE_UNAVAILABLE_MESSAGE}
   // }
-  return result;
- } catch (error) {
-  throw error
- }
-}
-
-checkUsername = async (username) => {
- try {
-  const result = await mongoQuery.getDetails(USERS, [{ $match: { username } }]);
-  return result;
- } catch (error) {
-  throw error
- }
-}
-
-checkEmail = async (email) => {
- try {
-  const result = await mongoQuery.getDetails(USERS, [{ $match: { email } }]);
   return result;
  } catch (error) {
   throw error
@@ -58,7 +40,7 @@ exports.forgetPassword = async (reqParams) => {
   let _id = userData[0]['_id'];
   password = await pbkdf.hashPassword(password);
   let result = await mongoQuery.updateOne(USERS, { _id }, { password });
-  const otp = await otp({username,email})
+  const otp = await otp({ username, email })
   return result;
  } catch (error) {
   throw error
@@ -72,15 +54,18 @@ exports.login = async (reqParams) => {
   if (userData.length == 0) {
    return { 'status': NOT_FOUND_CODE, 'msg': "No account found!!!" };
   }
-  const userPwd = userData[0]["password"]
+  userObj = userData[0]
+  const userPwd = userObj["password"]
   const checkPwdStatus = await pbkdf.checkPassword(password, userPwd)
   if (checkPwdStatus == false) {
    return { "status": AUTH_ERROR_CODE, "msg": "Incorrect password" }
   }
-  delete userData[0]["password"]
-  const tokenRes = await jwt.generateToken(userData[0])
-  if (tokenRes){
-   return { "msg": "Login successful", "data": userData[0], "token": tokenRes };
+  userObj["user_id"] = userObj["_id"]
+  delete userObj["_id"]
+  delete userObj["password"]
+  const tokenRes = await jwt.generateToken(userObj)
+  if (tokenRes) {
+   return { "msg": "Login successful", "data": userObj, "token": tokenRes };
   } else {
    return { "msg": tokenRes['msg'], "status": tokenRes["status_code"] };
   }
@@ -89,31 +74,49 @@ exports.login = async (reqParams) => {
  }
 }
 
-otpSender = async (requestBody) => {
+exports.verify = async (requestBody) => {
  try {
-   let data = {
-     username: requestBody["username"] || "",
-     mail: requestBody["email"],
-   };
-   let result = await otp.sendOTPEmail(data);
-   otpJson[data["mail"]] = result["otp"];
-   return { status: true, data: result };
+  let otp = requestBody["otp"];
+  let mail = requestBody["mail"];
+  if (otp == otpJson[mail]) {
+   delete otpJson[mail];
+   return { status: true, msg: "Verification Successfully!!!" };
+  } else {
+   return { status: true, msg: "Verification Failed!!!" };
+  }
  } catch (error) {
-   return { status: false, data: error };
+  return { status: false, data: error };
  }
 };
 
-exports.verify = async (requestBody) => {
+const checkUsername = async (username) => {
  try {
-   let otp = requestBody["otp"];
-   let mail = requestBody["mail"];
-   if (otp == otpJson[mail]) {
-     delete otpJson[mail];
-     return { status: true, msg: "Verification Successfully!!!" };
-   } else {
-     return { status: true, msg: "Verification Failed!!!" };
-   }
+  const result = await mongoQuery.getDetails(USERS, [{ $match: { username } }]);
+  return result;
  } catch (error) {
-   return { status: false, data: error };
+  throw error
+ }
+}
+
+const checkEmail = async (email) => {
+ try {
+  const result = await mongoQuery.getDetails(USERS, [{ $match: { email } }]);
+  return result;
+ } catch (error) {
+  throw error
+ }
+}
+
+const otpSender = async (requestBody) => {
+ try {
+  const data = {
+   username: requestBody["username"] || "",
+   mail: requestBody["email"]
+  };
+  const result = await otp.sendOTPEmail(data);
+  otpJson[data["mail"]] = result["otp"];
+  return { status: true, data: result };
+ } catch (error) {
+  return { status: false, data: error };
  }
 };
